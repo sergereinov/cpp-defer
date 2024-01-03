c80e28a7b0bbc50c6553d5a07f310d2c38f7dcb8
Serge Reinov <47950629+sergereinov@users.noreply.github.com>
2024-01-03 05:18:52 +0300

    add prohibit of copying

--- defer.h
+++ defer.h
@@ -11,7 +11,20 @@ License: MIT
 
 class defer_t
 {
+	std::vector<std::function<void()>> m_lifo;
+
 public:
+	defer_t() = default;
+	defer_t(defer_t&&) = default;
+
+	defer_t(const defer_t&) = delete;
+	defer_t& operator=(const defer_t&) = delete;
+
+	void operator += (std::function<void()>&& f)
+	{
+		m_lifo.emplace_back(std::forward<std::function<void()>>(f));
+	}
+
 	~defer_t()
 	{
 		for (auto it = m_lifo.crbegin(); it != m_lifo.crend(); it++)
@@ -19,12 +32,4 @@ public:
 			if (*it) (*it)();
 		}
 	}
-
-	void operator += (std::function<void()> &&f)
-	{
-		m_lifo.emplace_back(std::move(f));
-	}
-
-private:
-	std::vector<std::function<void()>> m_lifo;
 };
